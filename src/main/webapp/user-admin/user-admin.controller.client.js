b52aef25f7567d9c152981faa6ca2d8c524c571f
Pablo Forero <ps.gomezforero@gmail.com>
2021-02-09 15:51:32 -0500

    Added template JS file

---
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,41 @@
+(function (){
+    var $usernameFld, $passwordFld;
+    var $firstNameFld, $lastNameFld, $roleFld;
+    var $removeBtn, $editBtn, $createBtn;
+    var $userRowTemplate, $tbodt;
+    var userService = new AdminUserServiceClient();
+
+    $(main);
+
+    function main() {
+
+    }
+
+    function createUser() {
+
+    }
+
+    function deleteUser() {
+
+    }
+
+    function selectUser() {
+
+    }
+
+    function updateUser() {
+
+    }
+
+    function renderUsers(users) {
+
+    }
+
+    function findAllUsers() {
+
+    }
+
+    function findUsersById() {
+
+    }
+})();

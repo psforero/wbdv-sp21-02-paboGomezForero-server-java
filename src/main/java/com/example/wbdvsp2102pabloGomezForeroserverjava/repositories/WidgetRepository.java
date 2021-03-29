9e1817e56ec015a4af4fce0d4d2a9675ceabecb3
Pablo Forero <ps.gomezforero@gmail.com>
2021-03-29 19:18:34 -0400

    Finished refactoring service to work with repository

--- src/main/java/com/example/wbdvsp2102pabloGomezForeroserverjava/repositories/WidgetRepository.java
+++ src/main/java/com/example/wbdvsp2102pabloGomezForeroserverjava/repositories/WidgetRepository.java
@@ -1,7 +1,14 @@
 package com.example.wbdvsp2102pabloGomezForeroserverjava.repositories;
 
 import com.example.wbdvsp2102pabloGomezForeroserverjava.models.Widget;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
+// responsible for talking to the database
 public interface WidgetRepository extends CrudRepository<Widget, Long> {
+    @Query(value = "SELECT * FROM wbdv_sp21_schema.widgets WHERE topic_id=:tid", nativeQuery = true)
+    List<Widget> findWidgetsForTopic(@Param("tid") String topicId);
 }
--- src/main/java/com/example/wbdvsp2102pabloGomezForeroserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2102pabloGomezForeroserverjava/services/WidgetService.java
@@ -15,61 +15,47 @@ public class WidgetService {
     @Autowired
     WidgetRepository repository;
 
-    private List<Widget> widgets = new ArrayList<Widget>();
-    {
-        Widget w1 = new Widget(123l, "60480131104b9800172010fa", "HEADING", 1, "Welcome to widget list a");
-        Widget w2 = new Widget(234l, "60480131104b9800172010fa", "PARAGRAPH", 1, "I am the a1 paragraph");
-        Widget w3 = new Widget(345l, "60480131104b9800172010fa", "PARAGRAPH", 1, "And I am the a2 one");
-        Widget w4 = new Widget(456l, "60480136104b9800172010fc", "HEADING", 2, "Welcome to widget list b");
-        Widget w5 = new Widget(567l, "60480136104b9800172010fc", "PARAGRAPH", 1, "I am paragraph b1");
-        Widget w6 = new Widget(678l, "60480136104b9800172010fc", "PARAGRAPH", 1, "And I am paragraph b2");
-
-        widgets.add(w1);
-        widgets.add(w2);
-        widgets.add(w3);
-        widgets.add(w4);
-        widgets.add(w5);
-        widgets.add(w6);
-    }
-
     public List<Widget> findAllWidgets() {
-        return widgets;
+        return (List<Widget>) repository.findAll();
     }
 
     public List<Widget> findWidgetsForTopic(String topicId) {
-        List<Widget> ws = new ArrayList<Widget>();
-        for (Widget widget: widgets) {
-            if (widget.getTopicId().equals(topicId)) {
-                ws.add(widget);
-            }
-        }
-        return ws;
+        return repository.findWidgetsForTopic(topicId);
     }
 
     public Widget createWidget(String topicId, Widget widget) {
         widget.setTopicId(topicId);
-        widget.setId((new Date()).getTime());
-        widgets.add(widget);
-        return widget;
+        return repository.save(widget);
     }
 
     public Integer deleteWidget(Long widgetId) {
-        for(int i = 0; i < widgets.size(); i++) {
-            if(widgets.get(i).getId().equals(widgetId)) {
-                widgets.remove(i);
-                return 1;
-            }
-        }
-        return -1;
+        repository.deleteById(widgetId);
+        return 1;
     }
 
     public Integer updateWidget(Long widgetId, Widget widget) {
-        for(int i = 0; i < widgets.size(); i++) {
-            if(widgets.get(i).getId().equals(widgetId)) {
-                widgets.set(i, widget);
-                return 1;
-            }
+        Widget originalWidget = repository.findById(widgetId).get();
+
+        if (widget.getType() != null) {
+            originalWidget.setType(widget.getType());
         }
-        return -1;
+        if (widget.getSize() != null) {
+            originalWidget.setSize(widget.getSize());
+        }
+        if (widget.getText() != null) {
+            originalWidget.setText(widget.getText());
+        }
+        if (widget.getWidth() != null) {
+            originalWidget.setWidth(widget.getWidth());
+        }
+        if (widget.getHeight() != null) {
+            originalWidget.setHeight(widget.getHeight());
+        }
+        if (widget.getSrc() != null) {
+            originalWidget.setSrc(widget.getSrc());
+        }
+
+        repository.save(originalWidget);
+        return 1;
     }
 }
